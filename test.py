2e73d8b84ab68c4725529865070131ab3f845f95
siposbence <30337626+siposbence@users.noreply.github.com>
2021-10-28 13:54:31 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import pyfakewebcam
+
+# sudo modprobe v4l2loopback devices=2 exclusive_caps=1,1 video_nr=5,6 card_label="WebVideo","Video4FaceDetection"
 
 mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
@@ -8,6 +11,10 @@ mp_drawing = mp.solutions.drawing_utils
 DIM=(744, 600)
 K=np.array([[328.66960044713676, 0.0, 461.9005635813701], [0.0, 329.7390185115684, 296.8864597194912], [0.0, 0.0, 1.0]])
 D=np.array([[-0.03254196233072562], [0.058705976062386735], [-0.08592867131653371], [0.04283255216208141]])
+
+
+camera = pyfakewebcam.FakeWebcam('/dev/video5', 744, 600) #600, 744
+camera_alt = pyfakewebcam.FakeWebcam('/dev/video6', 744, 600)
 
 def undistort(img):
     x,y,_ = img.shape
@@ -19,7 +26,7 @@ def undistort(img):
     return undistorted_img
 
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
 width = 2048
 height = 1536
@@ -30,31 +37,24 @@ with mp_face_detection.FaceDetection(
     model_selection=0, min_detection_confidence=0.5) as face_detection:
   while cap.isOpened():
     success, image = cap.read()
-    #x,y,_ = image.shape
-    #w, h = [500, 300]
-    #image = image[int(x/2)-h:int(x/2)+h, int(y/2)-w:int(x/2)+w,:]
-    
     if not success:
       print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
       continue
     
     image = undistort(image)
-
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
+    #image.flags.writeable = False
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = face_detection.process(image)
+    #results = face_detection.process(image)
 
-    # Draw the face detection annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.detections:
-      for detection in results.detections:
-        mp_drawing.draw_detection(image, detection)
-    # Flip the image horizontally for a selfie-view display.
+    #image.flags.writeable = True
+    #if results.detections:
+    #  for detection in results.detections:
+    #    mp_drawing.draw_detection(image, detection)
     cv2.imshow('MediaPipe Face Detection', cv2.flip(image, 1))
-    if cv2.waitKey(5) & 0xFF == 27:
+    #print(image.shape)
+    camera.schedule_frame(cv2.flip(image, 1))
+    camera_alt.schedule_frame(cv2.flip(image, 1))
+    if cv2.waitKey(1) & 0xFF == 27:
       break
 cap.release()
+
